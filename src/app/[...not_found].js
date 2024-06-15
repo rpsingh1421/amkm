8779005a60c3e9ded266eb918e233662959304d8
ram pratap singh <codingrpsingh@gmail.com>
2024-06-16 01:47:14 +0530

    modifeied not-found again and global css

--- src/app/[...not_found].js
+++ src/app/[...not_found].js
@@ -1,14 +1,5 @@
 // src/app/[...not_found].js
 
-// import {notFound} from "next/navigation"
-
-// export default function NotFoundCatchAll() {
-//   notFound()
-// }
-
-
-
-
 // export async function generateStaticParams() {
 //   // Return an array of parameters for dynamic routes.
 //   // For example, if you have dynamic routes for not found pages, you can return an empty array.
