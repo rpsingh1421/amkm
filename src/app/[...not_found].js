9c4054fb0ee7ddf4d356c534b8ae5cfbae0877bb
ram pratap singh <codingrpsingh@gmail.com>
2024-06-16 01:35:03 +0530

    dynamic not-found

---
+++ src/app/[...not_found].js
@@ -0,0 +1,20 @@
+// import {notFound} from "next/navigation"
+
+// export default function NotFoundCatchAll() {
+//   notFound()
+// }
+
+
+// pages/[...not_found].js
+
+// export async function generateStaticParams() {
+//   // Return an array of parameters for dynamic routes.
+//   // For example, if you have dynamic routes for not found pages, you can return an empty array.
+//   return [];
+// }
+
+import {notFound} from "next/navigation"
+
+export default function NotFoundCatchAll() {
+  notFound()
+}
