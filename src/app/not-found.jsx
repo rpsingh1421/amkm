63467d0b42e0ec1da5b072408f63b24ce6af9d69
ram pratap singh <codingrpsingh@gmail.com>
2024-06-16 02:32:25 +0530

    modified final not found

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -5,16 +5,25 @@ import { headers } from 'next/headers'
 import { Box, Typography } from '@mui/material'
  
 export default async function NotFound() {
-  const headersList = headers()
-  const domain = headersList.get('host')
-  const data = await getSiteData(domain)
+ 
   return (
-    <Box>
-      <Typography variant='h2'>Not Found: {data.name}</Typography>
-      <Typography>Could not find requested resource</Typography>
-      <Typography>
-        View <Link href="/blog">all posts</Link>
-      </Typography>
+    <Box className="py-[40px] bg-[#fff] font-serif">
+        <Box className="">
+            <Box className="w-1/2 m-auto">	
+                <Box className="col-sm-12 ">
+                    <Box className="col-sm-10 col-sm-offset-1  text-center">
+                        <Box className="bg-[url('/page_404.gif')] bg-center h-[400px]">
+                            <Typography className="text-center text-6xl font-extrabold">404</Typography>
+                        </Box>
+                        <Box className="mt-[-50px]">
+                            <Typography className="text-5xl font-bold"> Look like you're lost</Typography>
+                            <Typography className='text-xl my-[1%]'>the page you are looking for not avaible!</Typography>
+                            <Link href="/" className="inline-block text-[#fff] py-[10px] px-[20px] bg-[#39ac31] my-[20px]">Go to Home</Link>
+                        </Box>
+                    </Box>
+                </Box>
+            </Box>
+        </Box>
     </Box>
   )
 }
