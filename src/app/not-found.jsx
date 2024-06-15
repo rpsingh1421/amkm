b0bb90a42fefdc0518f8a9251a43e98dc7bb0b13
ram pratap singh <codingrpsingh@gmail.com>
2024-06-16 01:50:17 +0530

    modifeied not-found again and global css

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -5,12 +5,12 @@ import { headers } from 'next/headers'
 import { Box, Typography } from '@mui/material'
  
 export default async function NotFound() {
-//   const headersList = headers()
-//   const domain = headersList.get('host')
-//   const data = await getSiteData(domain)
+  const headersList = headers()
+  const domain = headersList.get('host')
+  const data = await getSiteData(domain)
   return (
     <Box>
-      {/* <Typography variant='h2'>Not Found: {data.name}</Typography> */}
+      <Typography variant='h2'>Not Found: {data.name}</Typography>
       <Typography>Could not find requested resource</Typography>
       <Typography>
         View <Link href="/blog">all posts</Link>
