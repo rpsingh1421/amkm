69f5a2d303f72fe767ae1ec680754d89a04326e2
ram pratap singh <codingrpsingh@gmail.com>
2024-06-16 06:24:31 +0530

    admin-panel extended

---
+++ src/app/admin-panel/dashboard/page.js
@@ -0,0 +1,15 @@
+"use client"
+import React from 'react'
+import AdminLayout from '../component/Layouts/AdminLayout'
+import { Typography } from '@mui/material'
+import Breadcrumb from '../component/Breadcumbs/Breadcumb'
+
+const DashBoard = () => {
+  return (
+    <AdminLayout>
+      <Breadcrumb pageName={'Dashboard'} />
+    </AdminLayout>
+  )
+}
+
+export default DashBoard
