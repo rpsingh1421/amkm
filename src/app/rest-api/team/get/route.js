ac883b1269aec9a02b704e4db4951f11b1663446
ram pratap singh <codingrpsingh@gmail.com>
2024-06-18 02:50:13 +0530

    first get api

--- src/app/rest-api/team/get/route.js
+++ src/app/rest-api/team/get/route.js
@@ -1,3 +1,5 @@
+import { NextResponse } from "next/server";
+
 export async function GET(request){
     return NextResponse.json({body:request,message:"team list fetched successfully",status:true},{status:200});
 }
