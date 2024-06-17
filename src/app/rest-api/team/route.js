a5b026fe4bba02b30840822768e8fa8f340d7641
ram pratap singh <codingrpsingh@gmail.com>
2024-06-18 02:36:08 +0530

    first get api

--- src/app/rest-api/team/route.js
+++ src/app/rest-api/team/route.js
@@ -1,5 +1,7 @@
+import connect from "@/utils/dbConnect";
 import { NextResponse } from "next/server";
 
+await connect();
 export async function GET(){
     return NextResponse.json({message:"hello from api",status:true});
 }
