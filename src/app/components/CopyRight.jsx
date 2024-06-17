6bbcb5315af78e859a3cff0d577349c6928b053b
ram pratap singh <codingrpsingh@gmail.com>
2024-06-17 14:41:17 +0530

    edited for fetching image from store

--- src/app/components/CopyRight.jsx
+++ src/app/components/CopyRight.jsx
@@ -1,11 +1,12 @@
 import { Box, Typography } from '@mui/material'
+import Link from 'next/link'
 import React from 'react'
 
 const CopyRight = () => {
   return (
     <Box className="bg-black text-center py-5">
         <Typography className="m-0 text-gray-300 text-base">
-            Copyright ©2024 All rights reserved | Developed by <a href="http://rpsinghdev.com/" target="_blank">R.P.Singh</a>
+            Copyright ©2024 All rights reserved | Developed by <Link href="http://rpsinghdev.com/" target="_blank">R.P.Singh</Link>
         </Typography>
     </Box>
   )
