9c0dc89a830d78b2d6809551bc0694b4b1950c59
ram pratap singh <codingrpsingh@gmail.com>
2024-06-18 03:22:05 +0530

    first get api

--- src/app/rest-api/team/route.js
+++ src/app/rest-api/team/route.js
@@ -1,6 +1,7 @@
 import connect from "@/utils/dbConnect";
 import { NextResponse } from "next/server";
 import TeamMemberModel from "../models/memberModel";
+import ContactDirectory from "../models/contactModel";
 
 await connect();
 export async function GET(){
@@ -8,6 +9,23 @@ export async function GET(){
     return NextResponse.json({body:teamList,message:"team list fetched successfully",status:true},{status:200});
 }
 export async function POST (request){
-  const receivedData = await request.json();
+    const receivedData = await request.json();
+    const registeredUser = await ContactDirectory.find({
+        $or: [
+            {
+                email:userData.member_email
+            },
+            {
+               contact: userData.contact
+            }
+        ]
+    });
+    if(registeredUser.length>0){
+        return NextResponse.json({message:'email or phone is already registered with us',status:false})
+    }
+    // else{
+
+    // }
+
   return NextResponse.json({cody:receivedData,message:"post data received successfully",status:true},{status:200})
 }
