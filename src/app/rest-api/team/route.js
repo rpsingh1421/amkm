3e39aba8fe48f4456a306637303838c0eba23516
ram pratap singh <codingrpsingh@gmail.com>
2024-06-18 02:40:44 +0530

    first get api

--- src/app/rest-api/team/route.js
+++ src/app/rest-api/team/route.js
@@ -1,7 +1,9 @@
 import connect from "@/utils/dbConnect";
 import { NextResponse } from "next/server";
+import TeamMemberModel from "../models/memberModel";
 
 await connect();
 export async function GET(){
-    return NextResponse.json({message:"hello from api",status:true});
+    const teamList= await TeamMemberModel.find();
+    return NextResponse.json({body:teamList,message:"team list fetched successfully",status:true},{status:200});
 }
