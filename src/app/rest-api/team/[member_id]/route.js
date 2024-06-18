0aa5de8ef99dde24355b808050d7c96ef73cbfb4
ram pratap singh <codingrpsingh@gmail.com>
2024-06-18 14:18:40 +0530

    team put api

--- src/app/rest-api/team/[member_id]/route.js
+++ src/app/rest-api/team/[member_id]/route.js
@@ -105,6 +105,33 @@ export async function PUT(request,{params}){
         }
     } catch (error) {
         return NextResponse.json({ status:false,message: "Error finding user" }, { status: 500 });
+    } 
+    //if team member exist and he wants to update phone or email ...
+    // then we have to check newly entered email or phone doesn,t registered with us already
+    
+    let registeredContactDetails;
+    if(teamMember.member_email != receivedData.member_email){
+        try {
+            registeredContactDetails = await ContactDirectory.find({email:receivedData.member_email})
+            if (registeredContactDetails) {
+                return NextResponse.json({status:false,message:`provided email:${receivedData.member_email} already registered`},{status:405})
+            }
+        } catch (error) {
+            return NextResponse.json({ status:false,message: "Error finding contatct directory by email" }, { status: 500 });
+        }
     }
-    return NextResponse.json({status:true,body:{id:_id,receivedData:receivedData},message:"editing team member"},{status:200})
+    if (teamMember.contact != receivedData.contact) {
+        try {
+            registeredContactDetails = await ContactDirectory.find({contact:receivedData.contact});
+            if (registeredContactDetails) {
+                return NextResponse.json({status:false,message:`provided phone:${receivedData.contact} already registered`},{status:405})
+            }
+        } catch (error) {
+            return NextResponse.json({ status:false,message: "Error finding contatct directory by phone" }, { status: 500 });
+        }
+    }
+    const updatedTeamMember =await TeamMemberModel.findByIdAndUpdate(_id,receivedData,{new:true});
+    return NextResponse.json({status:true,body:updatedTeamMember,message:"team member data updated successfully"},{status:200})
+   
+    // return NextResponse.json({status:true,body:{id:_id,receivedData:receivedData},message:"editing team member"},{status:200})
 }
