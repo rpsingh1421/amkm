58c5f91e3066131bd107543d7ee66d627a49b39f
ram pratap singh <codingrpsingh@gmail.com>
2024-06-20 04:14:33 +0530

    photo-gallery creroute modified

--- src/app/rest-api/photo-gallery/route.js
+++ src/app/rest-api/photo-gallery/route.js
@@ -11,3 +11,14 @@ export async function GET(){
     return NextResponse.json({status:false,message:'image list can,t be fetched',body:error.message},{status:200})
   }
 }
+export async function POST(request){
+  try {
+    const receivedData = await request.json();
+    const savedFilesData = await ImageGalleryModel.insertMany(receivedData);
+    console.log("received data",receivedData);
+    return NextResponse.json({status:true,message:'images saved successfully',body:savedFilesData},{status:200});
+  } catch (error) {
+    return NextResponse.json({status:false,error:error,message:'image not uploaded'},{status:400});
+  }
+  
+}
